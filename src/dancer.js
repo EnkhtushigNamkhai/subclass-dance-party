fa23ad03636f18b0b63b5e8ffc2957028929de94
Hack Reactor Student <student@hackreactor.com>
2017-10-16 17:23:18 -0700

    lineup finished and new dancers created and tested

--- src/dancer.js
+++ src/dancer.js
@@ -8,12 +8,12 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   this.timeBetweenSteps = timeBetweenSteps;
   this.top = top;
   this.left = left;
+  this.timer;
   // dancer.step = function() {
   //   // the basic dancer doesn't do anything interesting at all on each step,
   //   // it just schedules the next step
   //   setTimeout(dancer.step, timeBetweenSteps);
   // };
-  // makeDancer.prototype.step();
   this.step();
 
   // dancer.setPosition = function(top, left) {
@@ -29,15 +29,26 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  // this.setPosition(this.top, this.left);
+  this.setPosition(this.top, this.left);
   
 };
 
 
+makeDancer.prototype.lineup = function(i) {  
+  clearTimeout(this.timer);
+  this.top = 500;
+  this.left = ((i + 1) * 200);
+  let newStyle = {
+    top: this.top,
+    left: this.left
+  };
+  this.$node.css(newStyle);
+  console.log('hey, we are going to line up eventually');
+};
 
 makeDancer.prototype.step = function() {
   let fn = this.step.bind(this);
-  setTimeout(fn, this.timeBetweenSteps);
+  this.timer = setTimeout(fn, this.timeBetweenSteps);
 };
 
 makeDancer.prototype.setPosition = function(top, left) {
---
+++ src/wavyDancer.js
@@ -0,0 +1,59 @@
+var makeWavyDancer = function(top, left, timeBetweenSteps) { 
+  let topBoundary = 600 + (Math.random() * 100);
+  let leftBoundary = 100 + (Math.random() * 50);
+  makeDancer.call(this, topBoundary, leftBoundary, timeBetweenSteps);
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  // var oldStep = blinkyDancer.step;
+
+  // blinkyDancer.step = function() {
+  //   // call the old version of step at the beginning of any call to this new version of step
+  //   oldStep();
+  //   // toggle() is a jQuery method to show/hide the <span> tag.
+  //   // See http://api.jquery.com/category/effects/ for this and
+  //   // other effects you can use on a jQuery-wrapped html tag.
+  //   blinkyDancer.$node.toggle();
+  // }; 
+  this.down = false;
+  this.right = true;
+};
+
+makeWavyDancer.prototype = Object.create(makeDancer.prototype);
+
+//
+makeWavyDancer.prototype.step = function() {
+  makeDancer.prototype.step.call(this);
+  // go up and right
+  if (this.top < 200) {
+    this.down = true;
+  } else if (this.top > 900) {
+    this.down = false;
+  }
+
+  if (this.left > 800) {
+    this.right = false;
+  } else if (this.left < 200) {
+    this.right = true;
+  }
+
+  if (!this.down) {
+    this.top -= 60;
+  } else if (this.down) {
+    this.top += 60;
+  }
+
+  if (this.right) {
+    this.left += 60;
+  } else if (!this.right) {
+    this.left -= 60;
+  }
+
+  var newStyle = {
+    top: this.top,
+    left: this.left
+  };
+  this.$node.css(newStyle);
+};
+
+makeWavyDancer.prototype.constructor = makeWavyDancer;
